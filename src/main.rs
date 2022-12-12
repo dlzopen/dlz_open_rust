2adf8692a15383fd08b81ec91f7b8ab41af14713
dlzopen <dlzopen@163.com>
2022-12-12 15:29:26 +0800

    update for docker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+use rd_map::MapPlugin;
+
+
+
 fn main() {
     println!("你好，轰隆隆……");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(MapPlugin)
+        .run();
 }
