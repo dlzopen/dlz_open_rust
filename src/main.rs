9b73a519a1af7b9edb264c89940467f59977e9f4
dlzopen <dlzopen@163.com>
2022-12-12 19:18:50 +0800

    Revert "update for docker"
    This reverts commit 2adf8692a15383fd08b81ec91f7b8ab41af14713.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use bevy::prelude::*;
-use rd_map::MapPlugin;
-
-
-
 fn main() {
     println!("你好，轰隆隆……");
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(MapPlugin)
-        .run();
 }
